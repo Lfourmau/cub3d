ed960ab11cd612f6ed6c9f9aed59067b80d6fdd3
LoloLaD <loicfourmaux@gmail.com>
2021-02-15 15:26:15 +0100

    attention losanges et couleurs

--- minilibx_mms_20200219/main.c
+++ minilibx_mms_20200219/main.c
@@ -6,12 +6,19 @@
 /*   By: lfourmau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 09:30:31 by lfourmau          #+#    #+#             */
-/*   Updated: 2021/02/15 11:13:48 by lfourmau         ###   ########lyon.fr   */
+/*   Updated: 2021/02/15 15:24:08 by lfourmau         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
-
+void	ft_line(void *mlx_ptr, void* win_ptr, int axe, int a, int b, int color)
+{
+	while (a < b)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, axe, a, color);
+		a++;
+	}
+}
 int     main(void)
 {
 	int i = 0;
@@ -21,34 +28,54 @@ int     main(void)
 
     mlx_ptr = mlx_init();
     win_ptr = mlx_new_window(mlx_ptr, 900, 900, "TEEEEEEST");
-	mlx_string_put(mlx_ptr, win_ptr, 250, 250, 0*65536+255*256+0, "Bonsoir bonjour bon apres midi");
-	while (i < 60)
+	mlx_string_put(mlx_ptr, win_ptr, 305, 440, 255, "Bonsoir bonjour bon apres midi");
+	while (i < 900)
 	{
-		mlx_pixel_put(mlx_ptr, win_ptr, 160 + i, 160 + j, 255*65536+0*256+0);
-		i++;
-		j+=2;
-	}
-	i = 0;
-	j = 0;
-	while (i < 60)
-	{
-		mlx_pixel_put(mlx_ptr, win_ptr, 600 + i, 160 + j, 255*65536+0*256+0);
-		i++;
-		j+=2;
-	}
-	i = 0;
-	j = 0;
-	while (i < 400)
-	{
-		mlx_pixel_put(mlx_ptr, win_ptr, 170 + i, 170, 255*65536+0*256+0);
+		mlx_pixel_put(mlx_ptr, win_ptr, i, i, 0*65536+255*256+0);
 		i++;
 	}
 	i = 0;
-	j = 0;
-	while (i < 400)
+	while (i < 900)
 	{
-		mlx_pixel_put(mlx_ptr, win_ptr, 230 + i, 275, 255*65536+0*256+0);
+		mlx_pixel_put(mlx_ptr, win_ptr, 900-i, i, 0*65536+255*256+0);
 		i++;
 	}
+	i = 0;
+	while (i < 900)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, 450, i, 0*65536+255*256+0);
+		i++;
+	}
+	i = 0;
+	while (i < 900)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, i, 450, 0*65536+255*256+0);
+		i++;
+	}
+	i = 0;
+	while (i <450)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, 450 - i, i, 16711680);
+		i++;
+	}
+	i = 0;
+	while (i <450)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, 450 + i,  i, 16711680);
+		i++;
+	}
+	i = 0;
+	while (i <450)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, i,  450 + i, 16711680);
+		i++;
+	}
+	i = 0;
+	while (i <450)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, 900 - i,  450 + i, 16711680);
+		i++;
+	}
+    mlx_loop(mlx_ptr);
     mlx_loop(mlx_ptr);
 }
