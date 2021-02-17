eddd916c9108978f6943d5f7d6a5d942f4e2a474
LoloLaD <loicfourmaux@gmail.com>
2021-02-17 10:07:01 +0100

    add libft and .h begins

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 /*   By: lfourmau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 09:30:31 by lfourmau          #+#    #+#             */
-/*   Updated: 2021/02/15 15:24:08 by lfourmau         ###   ########lyon.fr   */
+/*   Updated: 2021/02/17 08:21:13 by lfourmau         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
+#include "cub3d.h"
 void	ft_line(void *mlx_ptr, void* win_ptr, int axe, int a, int b, int color)
 {
 	while (a < b)
@@ -28,6 +28,7 @@ int     main(void)
 
     mlx_ptr = mlx_init();
     win_ptr = mlx_new_window(mlx_ptr, 900, 900, "TEEEEEEST");
+//geometrie
 	mlx_string_put(mlx_ptr, win_ptr, 305, 440, 255, "Bonsoir bonjour bon apres midi");
 	while (i < 900)
 	{
@@ -76,6 +77,65 @@ int     main(void)
 		mlx_pixel_put(mlx_ptr, win_ptr, 900 - i,  450 + i, 16711680);
 		i++;
 	}
+
+//coloriage
+
+	i = 0;
+	while (i <450)
+	{
+		j = 0;
+		while (j < 900)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, j,  i, 30975);
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	while (i <450)
+	{
+		j = 0;
+		while (j < 900)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, j,  450 + i, 5266000);
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	while (i <100)
+	{
+		j = 800;
+		while (j < 900)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, j,  i, 13158450);
+			j++;
+		}
+		i++;
+	}
+	i = 95;
+	while (i < 200)
+	{
+		j = 400;
+		while (j < 700)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, j,  i, 16777215);
+			j++;
+		}
+		i++;
+	}
+	i = 95;
+	while (i < 250)
+	{
+		j = 75;
+		while (j < 250)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, j,  i, 16777215);
+			j++;
+		}
+		i++;
+	}
+
     mlx_loop(mlx_ptr);
     mlx_loop(mlx_ptr);
 }
