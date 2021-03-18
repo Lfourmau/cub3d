4cf184ce0f64f32d0008dd9ddfad1d155f825136
LoloLaD <loicfourmaux@gmail.com>
2021-03-18 15:20:53 +0100

    collision disabled, rotation enabled and try to rotate and move en meme time, trying to collision zeb

--- window_prog.c
+++ window_prog.c
@@ -6,7 +6,7 @@
 /*   By: lfourmau <lfourmau@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 09:00:44 by lfourmau          #+#    #+#             */
-/*   Updated: 2021/03/18 13:55:41 by lfourmau         ###   ########lyon.fr   */
+/*   Updated: 2021/03/18 15:20:02 by lfourmau         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,9 @@ int		render_next_frame(big_struct *bs)
 	bs->ws->img_ptr = mlx_get_data_addr(bs->ws->mlx_img, &bs->ws->bits_per_pixel, &bs->ws->line_length, &bs->ws->endian);
 	print_minimap(bs);
 	print_square(bs, bs->ws->player_pos_x, bs->ws->player_pos_y, 65280);
-	print_square(bs, bs->ws->player_pos_x + cos(bs->ws->p_angle) * bs->ws->multiplicator, bs->ws->player_pos_y + sin(bs->ws->p_angle) * bs->ws->multiplicator, 2971080);
+	print_square(bs, bs->ws->player_pos_x + cos(bs->ws->p_angle) * bs->ws->multiplicator, bs->ws->player_pos_y - sin(bs->ws->p_angle) * bs->ws->multiplicator, 2971080);
 	mlx_put_image_to_window(bs->ws->mlx_ptr, bs->ws->win_ptr, bs->ws->mlx_img, 0, 0);
 	mlx_destroy_image(bs->ws->mlx_ptr, bs->ws->mlx_img);
-	return (0);
-}
-
-int	rotation(int key, big_struct *bs)
-{
-	if (key == LEFTLOOK)
-		bs->ws->p_angle -= M_PI / 8;
-	if (key == RIGHTLOOK)
-		bs->ws->p_angle += M_PI / 8;
 	return (0);
 }
 
@@ -45,25 +36,19 @@ int	deal_key(int key, big_struct *bs)
 	if (key == ESC)
 		close_window(bs);
 	if (key == LEFTLOOK)
-		bs->ws->p_angle -= M_PI / 8;
-	if (key == RIGHTLOOK)
 		bs->ws->p_angle += M_PI / 8;
+	if (key == RIGHTLOOK)
+		bs->ws->p_angle -= M_PI / 8;
 	if (key == WKEY)
 	{
-		// if (bs->ms->map[bs->ws->player_pos_y / bs->ws->multiplicator - 1][bs->ws->player_pos_x / bs->ws->multiplicator ] != '1')
-		// {
-			bs->ws->player_pos_y += sin(bs->ws->p_angle) * bs->ws->multiplicator;
+			bs->ws->player_pos_y -= sin(bs->ws->p_angle) * bs->ws->multiplicator;
 			bs->ws->player_pos_x += cos(bs->ws->p_angle) * bs->ws->multiplicator;
-		// }
 		render_next_frame(bs);
 	}
 	if (key == SKEY)
 	{
-		// if (bs->ms->map[bs->ws->player_pos_y / bs->ws->multiplicator + 1][bs->ws->player_pos_x / bs->ws->multiplicator] != '1')
-		// {
-			bs->ws->player_pos_y -= sin(bs->ws->p_angle) * bs->ws->multiplicator;
+			bs->ws->player_pos_y += sin(bs->ws->p_angle) * bs->ws->multiplicator;
 			bs->ws->player_pos_x -= cos(bs->ws->p_angle) * bs->ws->multiplicator;
-		// }
 		render_next_frame(bs);
 	}
 	if (key == AKEY)
@@ -71,7 +56,7 @@ int	deal_key(int key, big_struct *bs)
 		// if (bs->ms->map[bs->ws->player_pos_y / bs->ws->multiplicator][bs->ws->player_pos_x / bs->ws->multiplicator - 1] != '1')
 		// {
 			bs->ws->player_pos_y -= sin(bs->ws->p_angle + (M_PI / 2)) * bs->ws->multiplicator;
-			bs->ws->player_pos_x -= cos(bs->ws->p_angle + (M_PI / 2)) * bs->ws->multiplicator;
+			bs->ws->player_pos_x += cos(bs->ws->p_angle + (M_PI / 2)) * bs->ws->multiplicator;
 		// }
 		render_next_frame(bs);
 	}
@@ -80,7 +65,7 @@ int	deal_key(int key, big_struct *bs)
 		// if (bs->ms->map[bs->ws->player_pos_y / bs->ws->multiplicator][bs->ws->player_pos_x / bs->ws->multiplicator + 1] != '1')
 		// {
 			bs->ws->player_pos_y += sin(bs->ws->p_angle + (M_PI / 2)) * bs->ws->multiplicator;
-			bs->ws->player_pos_x += cos(bs->ws->p_angle + (M_PI / 2)) * bs->ws->multiplicator;
+			bs->ws->player_pos_x -= cos(bs->ws->p_angle + (M_PI / 2)) * bs->ws->multiplicator;
 		// }
 		render_next_frame(bs);
 	}
